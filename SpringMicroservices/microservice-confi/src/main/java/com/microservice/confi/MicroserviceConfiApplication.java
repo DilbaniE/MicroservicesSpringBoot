fdf808f9567621916ccf3989f4f59ecf357c0684
DilbaniEB <claudia.enriquez@globant.com>
2024-04-01 18:38:27 -0500

    services the Eureka and Students

---
+++ SpringMicroservices/microservice-confi/src/main/java/com/microservice/confi/MicroserviceConfiApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.confi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfiApplication.class, args);
+	}
+
+}
